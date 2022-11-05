dd467b73a32670201efda76b82099bd99cac247b
Robert Bausdorf <rbausdorf@gmail.com>
2022-11-05 20:48:55 +0100

    build: Fix environment for tests.

--- src/test/java/de/bausdorf/simracing/irdataapi/client/StockDataToolsTest.java
+++ src/test/java/de/bausdorf/simracing/irdataapi/client/StockDataToolsTest.java
@@ -22,14 +22,17 @@ package de.bausdorf.simracing.irdataapi.client;
  * #L%
  */
 
-import de.bausdorf.simracing.irdataapi.model.CarClassKey;
-import de.bausdorf.simracing.irdataapi.model.CarInfoDto;
-import de.bausdorf.simracing.irdataapi.model.TrackInfoDto;
+import de.bausdorf.simracing.irdataapi.client.impl.IrDataClientImpl;
+import de.bausdorf.simracing.irdataapi.config.ConfigProperties;
+import de.bausdorf.simracing.irdataapi.model.*;
 import de.bausdorf.simracing.irdataapi.tools.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
 import java.util.*;
@@ -38,22 +41,22 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(classes = {IrDataClientTest.class})
+@EnableConfigurationProperties(value = ConfigProperties.class)
+@TestPropertySource("classpath:application-test.properties")
 @Slf4j
 class StockDataToolsTest {
 
-    static final StockDataCache dataCache = new StockDataCache(".cache");
+    @Autowired
+    ConfigProperties config;
 
-    @BeforeAll
-    static void fetchCacheData() {
-        try {
-            dataCache.fetchFromCache();
-        } catch (IOException e) {
-            log.warn(e.getMessage());
-        }
-    }
+    IrDataClient dataClient = new IrDataClientImpl();
+
+    StockDataCache dataCache = null;
 
     @Test
     void testGetTrackConfigMap() {
+        authenticate();
         Map<String, List<TrackInfoDto>> trackMap = StockDataTools.trackConfigurationMap(dataCache.getTracks());
         assertFalse(trackMap.isEmpty());
 
@@ -65,6 +68,7 @@ class StockDataToolsTest {
 
     @Test
     void testGetCarClassMap() {
+        authenticate();
         Map<CarClassKey, List<CarInfoDto>> carClassMap = StockDataTools.carClassMap(dataCache.getCarClasses(), dataCache.getCars());
         assertFalse(carClassMap.isEmpty());
 
@@ -76,6 +80,7 @@ class StockDataToolsTest {
 
     @Test
     void testCarsInClasses() {
+        authenticate();
         List<Long> classIds = Lists.list(3190L, 2523L);
         List<CarInfoDto> cars = StockDataTools.carsInClasses(classIds, dataCache.getCarClasses(), dataCache.getCars());
         assertFalse(cars.isEmpty());
@@ -85,6 +90,7 @@ class StockDataToolsTest {
 
     @Test
     void testFetchCarTypes() {
+        authenticate();
         NavigableSet<String> carTypes = StockDataTools.fetchAvailableCarTypes(dataCache.getCars(), MainCarType.ROAD);
         assertFalse(carTypes.isEmpty());
 
@@ -93,6 +99,7 @@ class StockDataToolsTest {
 
     @Test
     void testCarsByCategory() {
+        authenticate();
         List<CarInfoDto> carInfosWithLegacy = StockDataTools.carsByCategory(dataCache.getCars(), CarCategoryType.ROAD, false);
         assertFalse(carInfosWithLegacy.isEmpty());
 
@@ -102,6 +109,7 @@ class StockDataToolsTest {
 
     @Test
     void testCarsByType() {
+        authenticate();
         List<CarInfoDto> carInfosWithLegacy = StockDataTools.carsByType(dataCache.getCars(), "road", true);
         assertFalse(carInfosWithLegacy.isEmpty());
 
@@ -125,6 +133,7 @@ class StockDataToolsTest {
 
     @Test
     void testCarsByTypes() {
+        authenticate();
         List<String> carTypes = List.of(Constants.GT3, Constants.GT4, Constants.TCR, "supercup");
         List<CarInfoDto> carInfos = StockDataTools.carsByType(dataCache.getCars(), carTypes, true);
         assertFalse(carInfos.isEmpty());
@@ -134,6 +143,7 @@ class StockDataToolsTest {
 
     @Test
     void testTracksByType() {
+        authenticate();
         List<TrackInfoDto> infosWithLegacy = StockDataTools.tracksByType(dataCache.getTracks(), TrackType.ROAD, true);
         assertFalse(infosWithLegacy.isEmpty());
 
@@ -150,9 +160,33 @@ class StockDataToolsTest {
 
     @Test
     void testCarCategories() {
+        authenticate();
         NavigableSet<String> categories = new TreeSet<>();
         Arrays.stream(dataCache.getCars()).forEach(car -> categories.addAll(Arrays.stream(car.getCategories()).collect(Collectors.toList())));
         assertFalse(categories.isEmpty());
         categories.stream().sorted().forEach(log::info);
+    }
+
+    private void authenticate() {
+        if (!dataClient.isAuthenticated()) {
+            LoginRequestDto dto = LoginRequestDto.builder()
+                    .email(config.getUser())
+                    .password(config.getPassword())
+                    .build();
+            AuthResponseDto authResponseDto = dataClient.authenticate(dto);
+            dataClient.setLogResponseJson(config.getLogResponseJson());
+            log.info(authResponseDto.toString());
+            if (dataCache == null) {
+                dataCache = new StockDataCache(config.getCacheDirectory());
+                try {
+                    if (!dataCache.cacheExists()) {
+                        dataCache.fetchFromService(dataClient);
+                    }
+                    dataCache.fetchFromCache();
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+            }
+        }
     }
 }
